01024aa83974f08e7da39ccef760d11ea972ef41
Aishwaryalakshmi Arumugam <aishwaryalakshmi.a@gmail.com>
2021-10-15 22:33:37 +0100

    updated main

--- src/main/java/calculatorApp/Calculator.java
+++ src/main/java/calculatorApp/Calculator.java
@@ -19,11 +19,12 @@ public class Calculator {
 	}
 	
 	public static void main(String[] args) {
+		System.out.println("========================");
 		System.out.println(add(1, 2));
 		System.out.println(subtract(1, 2));
 		System.out.println(multiply(1, 2));
 		System.out.println(divide(1, 2));
-
+		System.out.println("========================");
 	}
 
 }
